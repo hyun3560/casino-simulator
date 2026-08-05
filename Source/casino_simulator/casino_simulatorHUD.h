a1baaeea8b1e751405960d97535817c02f94d180
cksgns032 <cksgns032@naver.com>
2026-08-05 17:55:06 +0900

    GAS , Player Hud 적용

---
+++ Source/casino_simulator/casino_simulatorHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "casino_simulatorHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CASINO_SIMULATOR_API Acasino_simulatorHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
